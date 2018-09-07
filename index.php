f29208492f9cf1087b3a258d48ab9e9b751ba17a
CYFPro <41472804+CYFPro@users.noreply.github.com>
2018-09-07 20:16:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html>
 <body>
-
-<h1>我的第一张 PHP 页面</h1>
+<title>油哥新博客!<title>
+<h1>油哥的第一个PHP界面,丑不丑?</h1>
 
 <?php
-echo "Hello World!";
+echo "油哥不会php啊,这个很棘手.";
+echo "努力一下吧，反正全用html也不是问题。"
 ?>
 
 </body>
