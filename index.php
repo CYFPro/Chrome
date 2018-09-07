9a1f41083c63a08e4435fcf60532f0bfdf1b0be7
CYFPro <41472804+CYFPro@users.noreply.github.com>
2018-09-07 20:21:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,9 @@
 
 <?php
 echo "油哥不会php啊,这个很棘手.";
-echo "努力一下吧，反正全用html也不是问题。"
+echo "努力一下吧，反正全用html也不是问题。";
 ?>
-
+</p>这个界面呢，主要放一些无关紧要，不会被Q的东西。</P>
+</P>Copyright 2018    Pig's oil.lnc, </P>
 </body>
 </html>
