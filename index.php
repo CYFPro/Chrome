467b634cc0e6a477d2fc37ca6f76c2ddb0e28f43
CYFPro <41472804+CYFPro@users.noreply.github.com>
2018-09-08 18:37:01 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,17 +1,45 @@
-<!DOCTYPE html>
-<html>
-<body>
-<title>油哥新博客!</title>
-  
-<h1>油哥的第一个PHP界面,丑不丑?</h1>
-
+﻿<!DOCTYPE html>
 <?php
-echo "油哥不会php啊,这个很棘手.";
-echo "努力一下吧，反正全用html也不是问题。";
+echo ""
 ?>
-</p>这个界面呢，主要放一些无关紧要，不会被Q的东西。</P>
-<a href=https://cyfblog.herokuapp.com/Attention.php> 注意注意!</P>
-</p>最后更新:2018-09-07</p>
-</P>Copyright 2018    Pig's oil.lnc, </P>
-</body>
-</html>
+<html lang="zh-CN">
+<head>
+<meta charset="utf-8" http-equiv="Content-Type" content="text/html" />
+<title>​猪油博客 | 猪油新博客</title>
+<body background="https://github.com/CYFPro/picturebed/raw/master/oil.png" ;background-repeat: no-repeat;>		
+<p align="center"><img src="https://github.com/CYFPro/picturebed/raw/master/logo.png" width="400" height="200" alt="​pig's oil" ></p>
+
+			</a></h1>
+<h1 align="center">猪油 | cyfblog.herokuapp.com</h1>
+		<a href="https://cyfblog.herokuapp.com" title="Click Menu" id="menu-trigger" class="menu-trigger"></a>
+	</div><script type="text/javascript">jQuery("#loadbar").animate({width:"66%",backgroundColor: '#1200ff'});</script>
+		<div class="wrap w-size" id="wrap">
+	
+<link rel="shortcut icon" href="https://github.com/CYFPro/picturebed/raw/master/icon.ico" /> 
+<div class="post-wrap" >
+		<div class="m-post">
+			
+
+<h2 class="title"><a href="https://cyfblog.herokuapp.com/Attention.php" title="注意注意!" rel="bookmark">注意注意!!</a></h2>
+				<div class="text">
+					<p style="text-align:center;"><a href="https://cyfblog.herokuapp.com/Attention.php" class="phzoom" title="注意注意"><img src="https://github.com/CYFPro/picturebed/raw/master/oil.png" alt="注意注意" /></a></p><p>
+关于上油博的注意事项[...]</p>				</div>
+							</div>
+		</div>
+		<div class="m-infoarea clearfix">
+			<a class="time" href="">2018年8月12日</a>
+												<a href="" class="comment" >8</a>			<span class="sep">/</span>
+			<div class="from"><span class="reblog"><a href="" rel="category tag">科学上网</a></span></div>
+			<span class="sep">/</span>
+			<div class="tagarea clearfix">
+				<span class="taglb">标签:&nbsp;</span>
+				科学上网			</div>
+		</div>
+	</div>										<div class="post-wrap" >
+		<div class="m-post">
+			<a class="typeicon standard" href="https://cyfblog.herokuapp.com/Attention.php"></a>
+			<div class="content">
+	
+<h2>我是一条可爱的分界线——————————————————————————————————————————————————————</h2>
+<P>CopyRight 2018 Pig's oil.lnc, </P>
+</head>			
