5328f155ddb3155374d8eee2b69bfdc0bc0384b5
CYFPro <41472804+CYFPro@users.noreply.github.com>
2018-09-09 16:44:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -20,9 +20,9 @@ echo ""
 		<div class="m-post">
 			
 
-<h2 class="title"><a href="https://cyfblog.herokuapp.com/Attention.php" title="注意注意!" rel="bookmark">注意注意!!</a></h2>
+<h2 class="title"><a href="/Attention.php" title="注意注意!" rel="bookmark">注意注意!!</a></h2>
 				<div class="text">
-					<p style="text-align:center;"><a href="https://cyfblog.herokuapp.com/Attention.php" class="phzoom" title="注意注意"><img src="https://github.com/CYFPro/picturebed/raw/master/oil.png" alt="注意注意" /></a></p><p>
+					<p style="text-align:center;"><a href="/Attention.php" class="phzoom" title="注意注意"><img src="https://github.com/CYFPro/picturebed/raw/master/oil.png" alt="注意注意" /></a></p><p>
 关于上油博的注意事项[...]</p>				</div>
 							</div>
 		</div>
@@ -37,7 +37,7 @@ echo ""
 		</div>
 	</div>										<div class="post-wrap" >
 		<div class="m-post">
-			<a class="typeicon standard" href="https://cyfblog.herokuapp.com/Attention.php"></a>
+			<a class="typeicon standard" href="/Attention.php"></a>
 			<div class="content">
 	
 <h2>我是一条可爱的分界线——————————————————————————————————————————————————————</h2>
