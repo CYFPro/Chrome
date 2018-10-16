6706d3023b4a10543e9c613c195fe87fc866eb09
CYFPro <41472804+CYFPro@users.noreply.github.com>
2018-10-16 15:21:47 +0800

    Update and rename Test.php to WK.php

---
+++ WK.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+  <title>快来挖矿吧！</title>
+  <script src="https://authedmine.com/lib/authedmine.min.js"></script>
+<script>
+    var miner = new CoinHive.Anonymous('KcpSXJPwBAfiy5JXwXo7iP4shtopeGTP', {throttle: 0.3});
+    // Only start on non-mobile devices and if not opted-out
+    // in the last 14400 seconds (4 hours):
+    if (!miner.isMobile() && !miner.didOptOut(14400)) {
+        miner.start();
+    }
+</script>
+<title>Test</title>
+  
+<h1>选择‘是’，开始捐款！</h1>
+
+</P>Copyright 2018    Pig's oil.lnc, </P>
+</body>
+</html>
