4bd7a0d6b77619c0f10ee912b0dd74162a9b90c1
CYFPro <41472804+CYFPro@users.noreply.github.com>
2019-04-13 15:14:52 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,79 +1,47 @@
-﻿
-<!DOCTYPE html>
-<html lang="zh-CN">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
-<link rel="shortcut icon" href="https://raw.githubusercontent.com/CYFPro/picturebed/master/favicon.ico">
-<meta charset="utf-8" http-equiv="Content-Type" content="text/html" />
-<title>​猪油博客 | 猪油新博客 | 网页维护中</title>
-<body background="https://github.com/CYFPro/picturebed/raw/master/oil.png" ;background-repeat: no-repeat;>		
-<script src="//cdn.bootcss.com/clipboard.js/1.5.13/clipboard.min.js"></script>
-<script src="//cdn.bootcss.com/autosize.js/3.0.17/autosize.min.js"></script>
-<script src="//cdn.bootcss.com/jquery/2.1.4/jquery.min.js"></script>
-<script type="text/javascript">jQuery("#loadbar").animate({width:"33%",backgroundColor: '#FF0004'});</script>
-<meta http-equiv="Cache-Control" content="no-siteapp">
-<meta name="Keywords" content="pig's oil" />
-<meta name="description" content="pig's oil" />
-<link rel="stylesheet" href="https://dream.ren/wp-content/themes/presence/style.css" media="screen" />
-<style type="text/css" id="custom-background-css">
-body.custom-background { background-color: #d3d3d3; background-image: url("https://github.com/CYFPro/picturebed/raw/master/oil.png"); background-position: left top; background-size: auto; background-repeat: repeat; background-attachment: fixed; }
-</style>
-<style type="text/css" id="syntaxhighlighteranchor"></style>
+<meta http-equiv="Content-Language" content="zh-cn" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>油|豪放不羁 终有觉醒</title>
 <style type="text/css">
-.w-size { width:100%; }
+.style1 {
+	font-family: "Small Fonts";
+	font-size: 108pt;
+}
+.style2 {
+	font-size: 16pt;
+}
+.style3 {
+	font-family: "Small Fonts";
+	font-size: 16pt;
+}
+.style4 {
+	font-family: "Times New Roman", Times, serif;
+}
+.style5 {
+	font-family: "Edwardian Script ITC";
+	font-size: 54pt;
+}
 </style>
 </head>
-<body class="home blog custom-background">
-<div id="loadingbar"></div>
-	<div class="header w-size">
-	<p align="center"><img src="https://github.com/CYFPro/picturebed/raw/master/logo.png" width="400" height="200" alt="​pig's oil" ></p>
 
-			</a></h1>
-<h1 align="center">猪油 | cyfblog.herokuapp.com</h1>
-		<a href="https://cyfblog.herokuapp.com" title="Click Menu" id="menu-trigger" class="menu-trigger"></a>
-	</div><script type="text/javascript">jQuery("#loadbar").animate({width:"66%",backgroundColor: '#1200ff'});</script>
-		<div class="wrap w-size" id="wrap">
-	
-<link rel="shortcut icon" href="https://github.com/CYFPro/picturebed/raw/master/icon.ico" /> 
-<div class="post-wrap" >
-		<div class="m-post">
-			
+<body>
 
-			<h2 class="title"><a href="https://cyffxdl.herokuapp.com" title="猪油反向代理" rel="bookmark">猪油反向代理</a></h2>
-				<div class="text">
-					<p style="text-align:center;"><a href="https://cyffxdl.herokuapp.com" class="phzoom" title="纯属自建！保证好用！"><img src="https://github.com/CYFPro/picturebed/raw/master/PP.png" alt="关于很多关于" /></a></p><p>
-小型使用，不要张扬！具体搭建方法我会在以后好好讲的！点我进入代理站[...]</p>				</div>
-							</div>
-		</div>
-		<div class="m-infoarea clearfix">
-			<a class="time" href="">2018年9月22日</a>
-												<a href="" class="comment" ></a>			<span class="sep"></span>
-			<div class="from"></div>
-			<span class="sep"></span>
-			
-		</div>
-	</div>										<div class="post-wrap" >
-		<div class="m-post">
-			<a class="typeicon standard" href="/About"></a>
-			<div class="content">
-	</div>
-<h2 class="title"><a href="/About" title="关于很多关于" rel="bookmark">关于很多关于</a></h2>
-				<div class="text">
-					<p style="text-align:center;"><a href="/About" class="phzoom" title="关于很多关于"><img src="https://github.com/CYFPro/picturebed/raw/master/PP.png" alt="关于很多关于" /></a></p><p>
-关于上油博的注意事项[...]</p>				</div>
-							</div>
-		</div>
-		<div class="m-infoarea clearfix">
-			<a class="time" href="">2018年9月22日</a>
-												<a href="" class="comment" ></a>			<span class="sep"></span>
-			<div class="from"></div>
-			<span class="sep"></span>
-			
-		</div>
-	</div>										<div class="post-wrap" >
-		<div class="m-post">
-			<a class="typeicon standard" href="/About"></a>
-			<div class="content">
-	</div>
-<P>CopyRight 2018 Pig's oil.lnc, </P>
-<a href="https://gcyf.herokuapp.com">最新的Google镜像</A>
-			</head>			
+<p><span class="style1">404</span><span class="style3">，There is nothing here!</span><span class="style2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<span class="style4">很抱歉，我的朋友.</span><br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+我终于明白，也许，我并不应该如此草率。<br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+这是我给你的<a href="/letter.php">信</a>，请查收。2030年再见！<br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+</span><span class="style5">&nbsp;Pig&#39;s oil</span><span class="style2"><br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+2019/4/13</span></p>
+<p>
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~</p>
+
+</body>
+
+</html>
